97ddeb02ad943d2dab4359129f2ee169cc45b552
rudgal <office@galler.digital>
2022-08-10 17:30:02 +0200

    Section 4 - Practice: 64. Creating, Using & Displaying Random Numbers

--- code/01-creating-custom-buttons/components/ui/PrimaryButton.js
+++ code/01-creating-custom-buttons/components/ui/PrimaryButton.js
@@ -1,5 +1,5 @@
 import { Pressable, Text, View, StyleSheet } from 'react-native';
-import Colors from '../constants/colors';
+import Colors from '../../constants/colors';
 
 function PrimaryButton({ children, onPress }) {
   return (
--- code/01-creating-custom-buttons/screens/StartGameScreen.js
+++ code/01-creating-custom-buttons/screens/StartGameScreen.js
@@ -1,6 +1,6 @@
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from '../components/ui/PrimaryButton';
 import { useState } from 'react';
 import Colors from '../constants/colors';
 
