f248e748ed42c710a072545b39102277c6e1d768
rudgal <office@galler.digital>
2022-08-10 18:59:00 +0200

    Section 4 - Practice: 73. Adding Logic to (Re-)Start Games & Displaying a Summary Screen

--- code/01-creating-custom-buttons/App.js
+++ code/01-creating-custom-buttons/App.js
@@ -12,6 +12,8 @@ import AppLoading from 'expo-app-loading';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
   const [fontsLoaded] = useFonts({
     'open-sans  ': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
@@ -30,6 +32,12 @@ export default function App() {
     setGameIsOver(true);
   }
 
+  function startNewGameHandler() {
+    setUserNumber(null);
+    // setGameIsOver(true);
+    setGuessRounds(0);
+  }
+
   let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
   if (userNumber) {
@@ -39,7 +47,13 @@ export default function App() {
   }
 
   if (gameIsOver && userNumber) {
-    screen = <GameOverScreen />;
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
   }
 
   return (
