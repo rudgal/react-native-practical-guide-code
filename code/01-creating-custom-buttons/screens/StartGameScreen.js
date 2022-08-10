6c48f1b2cbd6f0058ea96b1e1c2f528401bff789
rudgal <office@galler.digital>
2022-08-10 16:51:12 +0200

    Section 4 - Practice: 59. Switching Screens Programmatically

--- code/01-creating-custom-buttons/screens/StartGameScreen.js
+++ code/01-creating-custom-buttons/screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 import { useState } from 'react';
 
-function StartGameScreen() {
+function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
 
   function numberInputHandler(enteredText) {
@@ -25,7 +25,7 @@ function StartGameScreen() {
       );
       return;
     }
-    console.log('Valid number!');
+    onPickNumber(chosenNumber);
   }
 
   return (
