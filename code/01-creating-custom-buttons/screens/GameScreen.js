498b44b5b509cbc65cf7bc108693625eb753367e
rudgal <office@galler.digital>
2022-08-10 19:18:10 +0200

    Section 4 - Practice: 77. Finishing Touches

--- code/01-creating-custom-buttons/screens/GameScreen.js
+++ code/01-creating-custom-buttons/screens/GameScreen.js
@@ -28,7 +28,7 @@ function GameScreen({ userNumber, onGameOver }) {
 
   useEffect(() => {
     if (currentGuess === userNumber) {
-      onGameOver();
+      onGameOver(guessRounds.length);
     }
   }, [currentGuess, userNumber, onGameOver]);
 
@@ -88,7 +88,7 @@ function GameScreen({ userNumber, onGameOver }) {
           </View>
         </View>
       </Card>
-      <View>
+      <View style={styles.listContainer}>
         <FlatList
           data={guessRounds}
           renderItem={(itemData) => (
@@ -116,6 +116,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
   },
+  listContainer: { flex: 1, padding: 16 },
 });
 
 export default GameScreen;
