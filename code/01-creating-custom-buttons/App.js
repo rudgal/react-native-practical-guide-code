88421bb03454ccf620c9acecd4e97612128262f7
rudgal <office@galler.digital>
2022-08-10 17:02:43 +0200

    Section 4 - Practice: 61. Respecting Device Screen Restrictions with the SafeAreaView

--- code/01-creating-custom-buttons/App.js
+++ code/01-creating-custom-buttons/App.js
@@ -1,4 +1,4 @@
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, SafeAreaView } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -26,7 +26,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- code/01-creating-custom-buttons/screens/GameScreen.js
+++ code/01-creating-custom-buttons/screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function GameScreen(props) {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Game Screen!</Text>
+      {/*GUESS*/}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/*+*/}
+        {/*-*/}
+      </View>
+      <View>{/*LOG ROUNDS*/}</View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
 
 export default GameScreen;
