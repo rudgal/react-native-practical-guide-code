7021fb2c6dc3490b2bc0738d3b4d5349a57e1f6c
rudgal <office@galler.digital>
2022-08-10 14:45:40 +0200

    Section 4 - Practice: 49. Styling for Android & iOS

--- code/01-creating-custom-buttons/screens/StartGameScreen.js
+++ code/01-creating-custom-buttons/screens/StartGameScreen.js
@@ -1,15 +1,30 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from "react-native";
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
 
 export default StartGameScreen;
