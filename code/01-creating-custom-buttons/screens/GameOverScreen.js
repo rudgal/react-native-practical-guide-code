d6618de68634341b5029d0f102b36a939bbc20cd
rudgal <office@galler.digital>
2022-08-10 17:58:53 +0200

    Section 4 - Practice: 66. Checking for "Game Over"

---
+++ code/01-creating-custom-buttons/screens/GameOverScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function GameOverScreen() {
+  return (
+    <View>
+      <Text>GameOver</Text>
+    </View>
+  );
+}
+
+export default GameOverScreen;
