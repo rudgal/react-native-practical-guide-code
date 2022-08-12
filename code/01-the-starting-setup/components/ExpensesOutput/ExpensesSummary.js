3ae51a4d754132b3b50d768ef3d6885ba33498ce
rudgal <office@galler.digital>
2022-08-12 08:17:47 +0200

    Section 8 - Practice: 128. Creating Key App Components to Display Expenses

---
+++ code/01-the-starting-setup/components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-web';
+
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 Days</Text>
+      <Text>$177.95</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
