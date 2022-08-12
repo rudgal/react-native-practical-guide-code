993d6d2dc1bd98da1b7f7b96d1bb6d8b2fa88bbb
rudgal <office@galler.digital>
2022-08-12 09:58:22 +0200

    Section 8 - Practice: 133. Working on Expense List Items

--- code/01-the-starting-setup/components/ExpensesOutput/ExpensesList.js
+++ code/01-the-starting-setup/components/ExpensesOutput/ExpensesList.js
@@ -1,8 +1,9 @@
 import { FlatList } from 'react-native';
 import { Text } from 'react-native';
+import ExpenseItem from './ExpenseItem';
 
 function renderExpenseItem(itemData) {
-  return <Text>{itemData.item.description}</Text>;
+  return <ExpenseItem {...itemData.item} />;
 }
 function ExpensesList({ expenses }) {
   return (
